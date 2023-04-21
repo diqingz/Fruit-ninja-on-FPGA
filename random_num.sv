f5da56ec319c1583e1cb7720c163f6924462bdc5
Diqing Z <sz_olivia@163.com>
2023-04-20 22:00:17 -0500

    added lfsr module (random_num)

---
+++ random_num.sv
@@ -0,0 +1,20 @@
+module random_num( input logic clk, reset,
+						 output reg [7:0] out
+);
+
+reg [7:0] lfsr_reg = 8'hFFFF; // initial value
+
+always @(posedge clk) begin
+    if (reset) 
+	 begin
+        lfsr_reg <= 8'hFFFF;
+    end 
+	 else 
+	 begin
+        lfsr_reg <= {lfsr_reg[6:0], lfsr_reg[7] ^ lfsr_reg[5]}; // feedback
+    end
+end
+
+assign out = lfsr_reg;
+
+endmodule
